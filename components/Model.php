e64ee9d7ed69e0c255b518053f32df273fde4ce1
Танат <t.aitkaliev@newcontact.su>
2018-11-14 22:56:31 +0400

    Models

--- components/Model.php
+++ components/Model.php
@@ -5,10 +5,10 @@ namespace app\components;
 abstract class Model
 {
     abstract public static function getTableName();
+
     public function getAll(\PDO $link)
     {
         $tableName = self::getTableName();
-        $sql = $link->query('select * from $tableName' . $tableName);
-        $link->prepare($sql);
+        return $link->query('select * from $tableName' . $tableName)->fetchAll();
     }
 }
